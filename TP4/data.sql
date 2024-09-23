fe246b75d069ee6128d11ce231e28c99591b66a9
Farhane <abdessamad.farhane@univ-rouen.fr>
2024-09-23 14:33:09 +0200

    wip

--- TP4/data.sql
+++ TP4/data.sql
@@ -1,7 +1,6 @@
-CREATE DATANASE demosql;
+CREATE DATABASE demosql;
 USE demosql;
-CREATE TABLE myTable (id int AUTO_INCREMENT, name varchar(45) NOT NULL
-	, PRIMARY KEY (id) );
-INSERT INTO myTable (id, name) VALUES (NULL, 'bob');
+CREATE TABLE myTable (id int AUTO_INCREMENT, name varchar(45) NOT NULL, PRIMARY KEY (id) );
+INSERT INTO myTable (id, name) VALUES (null, 'bob');
 INSERT INTO myTable (id, name) VALUES (NULL, 'alice');
 INSERT INTO myTable (id, name) VALUES (NULL, 'john');
